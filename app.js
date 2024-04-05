4896257a0afc524eb99685a0f4091b96e2251f25
Bhavya <bhavyshah2612@gmail.com>
2024-04-05 19:56:48 -0400

    added findall in app

--- app.js
+++ app.js
@@ -5,14 +5,29 @@ const ejs = require("ejs");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
+const Restaurants = require('./model/Restaurants');
+
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+const DBURI = process.env.MONGO_URI;
+
+// conntect with the database
+mongoose.connect(DBURI)
+    .then((result) => {
+        app.listen(PORT, () => {
+            console.log("Server started on port: " + PORT); // if connected then only listen to PORT
+        })
+    })
+    .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('HOME');
+    Restaurants.findOne()
+        .then((result) => res.send(result))
+        .catch((err) => console.log(err));
 })
 
-app.listen(PORT, () => {
-    console.log("Server started on port: " + PORT);
+app.use('', (req, res) => {
+    res.status(400).send("404");
 })
